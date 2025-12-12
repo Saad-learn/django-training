ea078d6443676fa799e5e13b21ec8d3da260f15b
saad <kingsaads705@gmail.com>
2025-12-12 19:13:56 +0500

    django training

---
+++ djangotutorial/polls/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, Saad. You're at the polls index.")
+
+def detail(request, question_id):
+    retuen 
---
+++ secondday/home/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    # id = models.AutoField()
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+    image = models.ImageField()
+    file = models.FileField()
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    speed = models.IntegerField(default=50)
+    
