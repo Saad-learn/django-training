6bb4d12317628c30a84c2bf65728ad3d31ace860
saad <kingsaads705@gmail.com>
2026-01-12 16:12:28 +0500

    messages framework

--- CBVproject/books/views.py
+++ CBVproject/books/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.urls import reverse_lazy
+from django.contrib import messages
 from .forms import BookForm
 from .models import Book
 
@@ -15,10 +16,18 @@ class BookListView(ListView):
     template_name = "books/book_list.html"
     context_object_name = "books"
 
+    def get(self, request, *args, **kwargs):
+        messages.info(request, "Book list loaded successfully.")
+        return super().get(request, *args, **kwargs)
+
 class BookDetailView(DetailView):
     model = Book
     template_name = "books/book_detail.html"
     context_object_name = "book"
+
+    def get(self, request, *args, **kwargs):
+        messages.info(request, "Book Detail View.")
+        return super().get(request, *args, **kwargs)
 
 @method_decorator(login_required, name="dispatch")
 class BookCreateView(LoginRequiredMixin, CreateView):
@@ -27,6 +36,10 @@ class BookCreateView(LoginRequiredMixin, CreateView):
     template_name = "books/book_form.html"
     success_url = reverse_lazy("book_list")
 
+    def get(self, request, *args, **kwargs):
+        messages.info(request, "Book Created Successfully.")
+        return super().get(request, *args, **kwargs)
+
 @method_decorator(login_required, name="dispatch")
 class BookUpdateView(LoginRequiredMixin, UpdateView):
     model = Book
@@ -34,9 +47,17 @@ class BookUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "books/book_form.html"
     success_url = reverse_lazy("book_list")
 
+    def get(self, request, *args, **kwargs):
+        messages.info(request, "Book Updated Successfully.")
+        return super().get(request, *args, **kwargs)
+
+
 @method_decorator(login_required, name = "dispatch")
 class BookDeleteView(LoginRequiredMixin, DeleteView):
     model = Book
     template_name = "books/book_confirm_delete.html"
     success_url = reverse_lazy("book_list")
 
+    def get(self, request, *args, **kwargs):
+        messages.info(request, "Book Deleted")
+        return super().get(request, *args, **kwargs)
